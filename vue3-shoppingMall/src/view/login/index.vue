74c0c9662024e2014162337a90a2a05ac84ebfe8
丘文雍 <875682652@qq.com>
2022-07-16 23:35:34 +0800

    打包配置

---
+++ vue3-shoppingMall/src/view/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+       <p> 账号 <input type="text" ></input></p>
+       <p> 密码 <input type="password" name="" id=""></input></p>
+    <!-- <p>验证码 <input type="text" width="120px" /></p> -->
+    
+    </div>
+
+</template>
+<script lang="ts" setup>
+
+</script>
